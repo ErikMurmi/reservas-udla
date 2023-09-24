75b32fc9d077fd4393c909ceeb8fa2b9dcf03d1d
Tito Jaramilo <tito.jaramillo@fisagrp.com>
2023-09-23 21:32:49 -0500

    Cambios .gitignore y dependencias

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 // Inyección de dependencias
-//builder.Services.AddTransient<ReservasCanchasUdlaContext>();
+builder.Services.AddTransient<ReservasCanchasUdlaContext>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
